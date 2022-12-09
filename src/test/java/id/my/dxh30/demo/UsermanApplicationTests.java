63625b33f4f12e22016d5a39bd9e8a6138f60b00
DXH30 <dhs0223@gmail.com>
2022-12-10 05:32:01 +0700

    Change testname

--- src/test/java/id/my/dxh30/demo/UsermanApplicationTests.java
+++ src/test/java/id/my/dxh30/demo/UsermanApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Salttest1ApplicationTests {
+class UsermanApplicationTests {
 
 	@Test
 	void contextLoads() {
